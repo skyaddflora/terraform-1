e03bdfabde7df46fc9977be58694214f8b253860
skyaddflora <50687589+skyaddflora@users.noreply.github.com>
2019-07-11 16:33:11 -0700

    Update main.tf

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -1,17 +1,18 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  #access_key = "${var.access_key}"
+  #secret_key = "${var.secret_key}"
   region = "${var.region}"
 }
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
+   bucket = "flora-bucket-terraform-lab"
+   key = "terraform.tfstate"
    region = "us-east-1"
   }
 }
 
-resource "aws_s3_bucket" "tf-root-module-bucket" {
+resource "aws_s3_bucket" "flora-bucket-terraform-lab" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
   region = "${var.s3_bucket_region}"
