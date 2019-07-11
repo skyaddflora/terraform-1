5c84a85f644417fc10febd4c0a71e46548c96ceb
skyaddflora <50687589+skyaddflora@users.noreply.github.com>
2019-07-11 14:17:49 -0700

    change s3 bucket

--- exercise_1/variables.tf
+++ exercise_1/variables.tf
@@ -16,7 +16,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = "string"
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "rc-tf-bucket-training-exersize-1"
+  default     = "flora-bucket-terraform-lab"
   }
 
 variable "tag_env" {
