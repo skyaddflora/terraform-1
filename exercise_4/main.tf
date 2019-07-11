b333a19476e1b94d071579e6da6e97170c7b5ba4
skyaddflora <50687589+skyaddflora@users.noreply.github.com>
2019-07-11 16:53:11 -0700

    Update main.tf

--- exercise_4/main.tf
+++ exercise_4/main.tf
@@ -12,7 +12,7 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "tf-root-module-bucket" {
+resource "aws_s3_bucket" "flora-bucket-terraform-labt" {
   bucket = "${var.s3_bucket_name}"
   acl = "private"
   region = "${var.s3_bucket_region}"
